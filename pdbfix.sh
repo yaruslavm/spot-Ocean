e1059896d5d92f3ef4145022f043c7b7ca2b3fa8
yaruslavm <yaruslav@hotmail.com>
2024-11-06 13:59:04 +0200

    Update pdbfix.sh

--- pdbfix.sh
+++ pdbfix.sh
@@ -1,19 +1,19 @@
 # Define the PDB name you're searching for
 PDB_NAME="spot-admission"
- 
+
 # Find the namespace containing the PDB
 NAMESPACE=$(kubectl get pdb --all-namespaces -o jsonpath="{.items[?(@.metadata.name=='$PDB_NAME')].metadata.namespace}")
- 
+
 # Check if the PDB was found
 if [ -z "$NAMESPACE" ]; then
   echo "PDB '$PDB_NAME' not found in any namespace."
 else
   echo "PDB '$PDB_NAME' found in namespace '$NAMESPACE'. Deleting..."
-  
+
 # Delete the PDB
 kubectl delete pdb $PDB_NAME -n $NAMESPACE
 
-# Create new PDB in 
+# Create new PDB in
 cat <<EOF | kubectl create -f - apiVersion: policy/v1 kind: PodDisruptionBudget metadata: namespace: kube-system name: spot-admission-controller-pdb spec: minAvailable: 1 selector: matchLabels: app.kubernetes.io/name: spot-admission-controller EOF
 
 fi
